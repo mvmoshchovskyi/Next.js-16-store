fedb5bccdc6eb9b97702022ad441f9683451699d
Mykhailo Moshchovskyi <mykhailo.m@vivint.com>
2024-09-02 10:30:32 +0300

    Shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
+import {Button} from "@/components/ui/button";
+
 function HomePage() {
-    return <h1 className='text-3xl'>HomePage</h1>;
+    return (
+        <div>
+            <h1 className='text-3xl'>HomePage</h1>
+            <Button>Click me</Button>
+        </div>
+    )
 }
 export default HomePage;
