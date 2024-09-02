f0aa2af5be9f082a3129a41cf769014ce9b524df
Mykhailo Moshchovskyi <mykhailo.m@vivint.com>
2024-09-02 11:05:52 +0300

    Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,30 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
+import Navbar from '@/components/navbar/Navbar';
+import Container from '@/components/global/Container';
+import Providers from './providers';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Next StoreFront",
-  description: "A nifty store build by Next.js",
+    title: "Next StoreFront",
+    description: "A nifty store build by Next.js",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body className={inter.className}>
+        <Providers>
+            <Navbar/>
+            <Container className='py-20'>{children}</Container>
+        </Providers>
+        </body>
+        </html>
+    );
 }
